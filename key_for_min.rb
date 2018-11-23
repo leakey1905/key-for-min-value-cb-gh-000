3d9879e88267391f9185ad8f8c5a2b006c6d4305
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 14:22:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.size == 0
     return nil
   else
-    min_hash_value = -10 ** 80
+    min_hash_value = 10 ** 80
     min_hash_key = -1
     name_hash.collect do |k, v|
       min_hash_value, min_hash_key = v, k unless v < min_hash_value
